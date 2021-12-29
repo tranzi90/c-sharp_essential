966017dc872f775794b392608416674b6c917808
denis_levchenko <tranzi90@gmail.com>
2021-12-29 21:35:56 +0300

    remove comments

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace Abstraction
 {
     abstract class BaseClass
     {
-        public abstract void Method(); // => Console.WriteLine("BaseClass.Method");
+        public abstract void Method();
         public abstract string Property { get; /*{ return "BaseClass.Property"; }*/ }
         public abstract string this[int index] { get; /*{ return "BaseClass.Indexer"; }*/ }
     }
